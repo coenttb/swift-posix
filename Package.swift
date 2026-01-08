375042826bef5778366e097ae96c12b305598522
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-08 08:34:08 +0100

    Update swift-kernel-primitives to 0.2.1

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/coenttb/swift-kernel-primitives.git", from: "0.2.0"),
+        .package(url: "https://github.com/coenttb/swift-kernel-primitives.git", from: "0.2.1"),
         .package(url: "https://github.com/swift-standards/swift-standards.git", from: "0.29.0")
     ],
     targets: [
